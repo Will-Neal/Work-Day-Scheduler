3a315d4e45a82b8078b0a4dd202d997fadad7dab
Will Neal <willwneal@gmail.com>
2022-01-28 17:16:55 -0500

    cleaned up old console logs and unused code for legibility, took new screenshot that better shows functionality. Temporarily adjusted the time via moment since it is late and doesnt demonstrate functionality well.

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,12 @@
-// console.log("Linked")
-
-
 //Moment based time that keeps current time
 $(document).ready(function(){
     updateTime()
     loadSaved()
     changeBoxColor()
-    // console.log("the ready function is running")
 })
 
 function updateTime() {
-    $("#currTime").text(moment().format('MMMM Do YYYY, h:mm:ss a'))
-    // console.log("the updatetime function is running")
+    $("#currTime").text(moment().add(-4, "hours").format('MMMM Do YYYY, h:mm:ss a'))
 };
 
 var timer = setInterval(updateTime, 1000);
@@ -21,8 +16,6 @@ var timer = setInterval(updateTime, 1000);
 
 //function to write the data saved in local storage to the input areas
 function loadSaved() {
-    // $("#nine").text(localStorage.getItem("9"))
-    // document.querySelector("#ten").textContent = "TEST"
     $("#nine").text(localStorage.getItem("9"))
     $("#ten").text(localStorage.getItem("10"))
     $("#eleven").text(localStorage.getItem("11"))
@@ -34,71 +27,52 @@ function loadSaved() {
     $("#seventeen").text(localStorage.getItem("17"))
 }
 
-// loadSaved()
 
 
 //Add event listener to each button 
 //Save text to local storage with each click - get value from textarea, then add key and save to local storage
 //writing a function for each save button
 $(".saveBtn9").click(function(){
-    // alert("you clicked the 9am save button")
-    // var hourKey = $(".hour9").val(); don't need because this is hardcoded to 9
     var textValue = $("#nine").val();
     localStorage.setItem("9", textValue)
 })
 
 $(".saveBtn10").click(function(){
-    // alert("you clicked the 10am save button")
-    // var hourKey = $(".hour9").val(); don't need because this is hardcoded to 9
     var textValue = $("#ten").val();
     localStorage.setItem("10", textValue)
 })
 
 $(".saveBtn11").click(function(){
-    // alert("you clicked the 11am save button")
-    // var hourKey = $(".hour9").val(); don't need because this is hardcoded to 9
     var textValue = $("#eleven").val();
     localStorage.setItem("11", textValue)
 })
 
 $(".saveBtn12").click(function(){
-    // alert("you clicked the 12pm save button")
-    // var hourKey = $(".hour9").val(); don't need because this is hardcoded to 9
     var textValue = $("#twelve").val();
     localStorage.setItem("12", textValue)
 })
 
 $(".saveBtn13").click(function(){
-    // alert("you clicked the 1pm save button")
-    // var hourKey = $(".hour9").val(); don't need because this is hardcoded to 9
     var textValue = $("#thirteen").val();
     localStorage.setItem("13", textValue)
 })
 
 $(".saveBtn14").click(function(){
-    // alert("you clicked the 2pm save button")
-    // var hourKey = $(".hour9").val(); don't need because this is hardcoded to 9
     var textValue = $("#fourteen").val();
     localStorage.setItem("14", textValue)
 })
 
 $(".saveBtn15").click(function(){
-    // alert("you clicked the 3pm save button")
-    // var hourKey = $(".hour9").val(); don't need because this is hardcoded to 9
     var textValue = $("#fifteen").val();
     localStorage.setItem("15", textValue)
 })
 
 $(".saveBtn16").click(function(){
-    // alert("you clicked the 4pm save button")
-    // var hourKey = $(".hour9").val(); don't need because this is hardcoded to 9
     var textValue = $("#sixteen").val();
     localStorage.setItem("16", textValue)
 })
 
 $(".saveBtn17").click(function(){
-    // alert("you clicked the 5pm save button")
-    // var hourKey = $(".hour9").val(); don't need because this is hardcoded to 9
     var textValue = $("#seventeen").val();
     localStorage.setItem("17", textValue)
 })
@@ -112,36 +86,21 @@ $("#clearBtn").click(function(){
 
 //Change box color
 
-function changeBoxColor() {
-// var hour = (moment().format("H"));
-// var hour = 9    
-// console.log(hour)    
+function changeBoxColor() {  
     $("textarea").each(function () {
-        // console.log(this)
-        // console.log(this.dataset.timeslot)
-        var hourStr = (moment().add(0, "hours").format("H")) // use add function to test different times, 0 is the present
+        var hourStr = (moment().add(-4, "hours").format("H")) // use add function to test different times, 0 is the present
         var hour = Number(hourStr)
-        // console.log(hour)
-        // var hour = 12
-        console.log("hour " + typeof(hour))
         calendarHourStr = this.dataset.timeslot;
         calendarHour = Number(calendarHourStr)
-        // calenderHourString = calendarHour.toString()
         console.log("calendarHour "+ typeof(calendarHour))
         if (calendarHour < hour) {
-            // console.log(calendarHour)
-            // console.log(hour)
             $(this).addClass("past")
             $(this).css("text-decoration", "line-through")
             $(this).removeClass("future")
         }   else if (calendarHour === hour) {
-            // console.log(calendarHour)
-            // console.log(hour)
             $(this).addClass("present");
         }   else {
            $(this).addClass("future")
         }
     })
 }
-
-// changeBoxColor()
